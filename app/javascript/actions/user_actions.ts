eef8e61bef9b5704011afe84ac6e609aa08ad400
1412320 <quocminh97@gmail.com>
2017-12-30 23:16:26 +0700

    Add signup actions

--- app/javascript/actions/user_actions.ts
+++ app/javascript/actions/user_actions.ts
@@ -1,3 +1,48 @@
 import { UserContants } from '../contants';
 import { history } from 'history';
 import { authenHeader } from '../helpers';
+import { userServices } from '../services';
+import { alertActions } from './';
+
+export const UserActions = {
+  signup
+}
+
+export function signup(user) {
+  return dispatch => {
+    dispatch(request(user));
+      userServices.signup(user)
+        .then(
+          user => {
+            dispatch(success(user));
+            history.push('/users/signup');
+            dispatch(alertActions.success('Signup successful'));
+          },
+          error => {
+            dispatch(failure(error));
+            dispatch(alertActions.error(error));
+          }
+        );
+  }
+
+  function request(user) {
+    return {
+      type: UserContants.SIGNUP_REQUEST,
+      user
+    }
+  }
+
+  function success(user) {
+    return {
+      type: UserContants.SIGN_UP_SUCCESS,
+      user
+    }
+  }
+
+  function failure(error) {
+    return {
+      type: UserContants.SIGNUP_FAILURE,
+      error
+    }
+  }
+}
--- app/javascript/services/user_services.ts
+++ app/javascript/services/user_services.ts
@@ -1,14 +1,20 @@
 import { authenHeader } from '../helpers';
 import axios from 'axios';
 
+export const userServices = {
+  signup
+}
+
 function signup(user) {
+  let response
   axios.post('/users/signup', {
     body: JSON.stringify(user)
   })
-  .then(function(response) {
-    return response;
+  .then(function(r) {
+    response = r;
   })
   .catch(function(error) {
-    return error;
+    return Promise.reject(error);
   })
+  return Promise.resolve(response);
 }
