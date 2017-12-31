3d97facf05d1588e62475dc11d57b54c19a6e702
1412321 <minhluong96@gmail.com>
2017-12-31 19:13:12 +0700

    Seed User & Wallet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171231101805) do
+ActiveRecord::Schema.define(version: 20171231110750) do
 
   create_table "blocks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "hash_str"
