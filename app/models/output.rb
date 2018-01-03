f01ba9a59b10c4558a833a03799d20a24b73e420
1412321 <minhluong96@gmail.com>
2018-01-03 15:35:09 +0700

    Calc Available Amount

--- app/models/output.rb
+++ app/models/output.rb
@@ -1,3 +1,18 @@
 class Output < ApplicationRecord
   belongs_to :trans, foreign_key: :output_ref, primary_key: :hash_str, class_name: 'Transaction'
+
+  def self.get_confirm_receive(wallet_hash)
+    includes(:trans).where(
+      transactions: { is_confirm: true },
+      receiver: wallet_hash 
+    )
+  end
+
+  def self.get_confirm_send(wallet_hash)
+    includes(:trans).where(
+      transactions: { is_confirm: true },
+      sender: wallet_hash 
+    )
+  end
+  
 end
