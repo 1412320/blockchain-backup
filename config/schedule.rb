e7b37391f46a5944af89ade9e0da6cf3221d876b
1412321 <minhluong96@gmail.com>
2018-01-03 17:33:31 +0700

    Run Crontab for Syncing with kcoin blockchain

---
+++ config/schedule.rb
@@ -0,0 +1,20 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+rails_env = ENV['RAILS_ENV'] || "development"
+set :environment, rails_env
+every 5.minutes do
+  rake "kcoin:sync"
+end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+# Learn more: http://github.com/javan/whenever
