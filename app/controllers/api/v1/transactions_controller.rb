862038ecf31193fdb87b8eb3df953ca0be7c2198
1412321 <minhluong96@gmail.com>
2018-01-06 16:08:03 +0700

    Refactor code Create transaction in service

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,13 +1,8 @@
 class Api::V1::TransactionsController < ApplicationController
+  before_action :init_service
   def create
-    @sender = current_user.wallet.address
-    @receiver = params[:receiver]
-    @amount = params[:amount]
-    @outputs = Output.usable(@sender)
-    @availble_amount = @outputs.sum(:amount)
-    if @availble_amount >= @amount
-      trans = create_transaction
-      res = KcoinTransaction.create(trans, current_user.wallet.private_key)
+    res = @service.create_transaction(params[:receiver], params[:amount])
+    if res
       if res.code == "200"
         KcoinTransaction.syncing_transaction
         render json:{ message: "Transaction created" }, status: 201
@@ -22,36 +17,12 @@ class Api::V1::TransactionsController < ApplicationController
 
   def index
     per_page = params[:per_page] || PER_PAGE
-    @transactions = KcoinService.new.get_lastest per_page: per_page
+    @transactions = @service.get_lastest_transactions per_page: per_page
     render json: { data: @transactions, total: @transactions.length }, status: 200    
   end
 
-  private
-  def create_transaction    
-    inputs = @outputs.map do |o|
-    {
-      referencedOutputHash: o.output_ref,
-      referencedOutputIndex: o.output_index
-    }
-    end
-
-    outputs = [
-      {
-        value: @amount,
-        lockScript: "ADD #{@receiver}"
-      }
-    ]
-    if @availble_amount > @amount
-      outputs << {
-        value: @availble_amount - @amount,
-        lockScript: "ADD #{@sender}"
-      }
-    end
-
-    return { 
-      inputs: inputs,
-      outputs: outputs,
-      version: 1 
-    }
+  private 
+  def init_service
+    @service = KcoinService.new current_user
   end
 end
--- app/services/kcoin_service.rb
+++ app/services/kcoin_service.rb
@@ -1,9 +1,24 @@
 require ('crawlers/kcoin_crawler')
 class KcoinService
-  def initialize
+  def initialize(user)
+    @user = user
   end
 
-  def get_lastest(per_page: 20)
+  def create_transaction(receiver, amount)
+    @sender = @user.wallet.address
+    @receiver = receiver
+    @amount = amount
+    @outputs = Output.usable(@sender)
+    @availble_amount = @outputs.sum(:amount)
+    if @availble_amount >= @amount
+      trans = init_transaction
+      res = KcoinTransaction.create(trans, @user.wallet.private_key)
+    else
+      return nil
+    end
+  end
+
+  def get_lastest_transactions(per_page: 20)
     @per_page = per_page
     @transactions = []
     data = KcoinCrawler.get_data("/blocks", order: -1, limit: @per_page)
@@ -31,5 +46,33 @@ class KcoinService
       @transactions << trans
       break if @transactions.length >= @per_page
     end
+  end
+
+  def init_transaction
+    inputs = @outputs.map do |o|
+      {
+        referencedOutputHash: o.output_ref,
+        referencedOutputIndex: o.output_index
+      }
+      end
+  
+      outputs = [
+        {
+          value: @amount,
+          lockScript: "ADD #{@receiver}"
+        }
+      ]
+      if @availble_amount > @amount
+        outputs << {
+          value: @availble_amount - @amount,
+          lockScript: "ADD #{@sender}"
+        }
+      end
+  
+      return { 
+        inputs: inputs,
+        outputs: outputs,
+        version: 1 
+      }
   end
 end
