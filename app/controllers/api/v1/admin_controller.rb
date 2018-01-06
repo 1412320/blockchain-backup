e09f2982b3bcb895fc5707cf0bd1cc0914b4f252
1412350 <ngoc120742@gmail.com>
2018-01-07 01:14:41 +0700

    authorize admin permission

--- app/controllers/api/v1/admin_controller.rb
+++ app/controllers/api/v1/admin_controller.rb
@@ -1,4 +1,5 @@
 class Api::Vcurrent_user.id::AdminController < ApplicationController
+  before_action :has_permission_to_do
   def all_user_infos
     @wallets = Wallet.where("user_id != ?", current_user.id )
     wallets_info = Array.new
@@ -16,5 +17,11 @@ class Api::Vcurrent_user.id::AdminController < ApplicationController
     system_real_amount = Output.real_amount.sum(:amount)
     system_available_amount = Output.available_amount.sum(:amount)
     render json: {user_count: user_count, system_real_amount: system_real_amount, system_available_amount: system_available_amount}
+  end
+  private
+  def has_permission_to_do
+    if (current_user.role != 0)
+      render json: {errors: "You don't have permission to do this action!"}, status: 401
+    end
   end
 end
