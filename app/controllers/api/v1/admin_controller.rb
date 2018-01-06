f7d0476efb4c81d7a235af5681a6418995bfaea2
1412350 <ngoc120742@gmail.com>
2018-01-07 01:30:27 +0700

    add api get address, real amount, available amount of each user

--- app/controllers/api/v1/admin_controller.rb
+++ app/controllers/api/v1/admin_controller.rb
@@ -1,16 +1,17 @@
-class Api::Vcurrent_user.id::AdminController < ApplicationController
+class Api::V1::AdminController < ApplicationController
   before_action :has_permission_to_do
   def all_user_infos
-    @wallets = Wallet.where("user_id != ?", current_user.id )
-    wallets_info = Array.new
-    @wallets.each do |wallet|
-      wallets_info.push({
-        address: wallet.address,
-        real_amount: wallet.real_amount,
-        available_amount: wallet.available_amount
+    @users = User.where("id != ?", current_user.id )
+    users_info = Array.new
+    @users.each do |user|
+      users_info.push({
+        user: user,
+        address: user.wallet.address,
+        real_amount: user.wallet.real_amount,
+        available_amount: user.wallet.available_amount
       })
     end
-    render json: wallets_info
+    render json: users_info
   end
   def system_info
     user_count = User.where("id != ?", current_user.id).count
