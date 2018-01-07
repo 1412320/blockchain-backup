054c571ad6dc665d394d901a12e7b0600f2169bb
1412350 <ngoc120742@gmail.com>
2018-01-07 11:29:19 +0700

    add admin reducers for get system info

--- app/javascript/reducers/admin_reducers.ts
+++ app/javascript/reducers/admin_reducers.ts
@@ -12,6 +12,17 @@ export function admin(state = {}, action) {
       }
     case AdminContants.ALL_USERS_INFO_FAILURE:
       return {}
+    case AdminContants.SYSTEM_INFO_REQUEST:
+    return {
+    }
+    case AdminContants.SYSTEM_INFO_SUCCESS:
+      return {
+        user_count: action.user_count,
+        system_real_amount: action.system_real_amount,
+        system_available_amount: action.system_available_amount     
+      }
+    case AdminContants.SYSTEM_INFO_FAILURE:
+      return {}
     default:
       return state;
   }
