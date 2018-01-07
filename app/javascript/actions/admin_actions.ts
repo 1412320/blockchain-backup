b55cc28c07dd0e2900a7e4cd0fb1866551939cc2
1412350 <ngoc120742@gmail.com>
2018-01-07 11:23:28 +0700

    add admin actions redux get system info

--- app/javascript/actions/admin_actions.ts
+++ app/javascript/actions/admin_actions.ts
@@ -3,7 +3,8 @@ import { adminServices } from '../services';
 import { alertActions } from './';
 
 export const adminActions = {
-  getAllUsersInfo
+  getAllUsersInfo,
+  getSystemInfo,
 }
 function getAllUsersInfo(page) {
   return dispatch => {
@@ -31,6 +32,44 @@ function getAllUsersInfo(page) {
     return {
       type: AdminContants.ALL_USERS_INFO_SUCCESS,
       users
+    }
+  }
+
+  function failure(error) {
+    return {
+      type: AdminContants.ALL_USERS_INFO_FAILURE,
+      error
+    }
+  }
+}
+function getSystemInfo() {
+  return dispatch => {
+    dispatch(request());
+    adminServices.getSystemInfo()
+        .then(
+          (response) => {
+            dispatch(success(response));
+            dispatch(alertActions.success("success"));
+          },
+          error => {
+            dispatch(failure(error));
+            dispatch(alertActions.error(error));
+          }
+        );
+  }
+
+  function request() {
+    return {
+      type: AdminContants.ALL_USERS_INFO_REQUEST,
+    }
+  }
+
+  function success(user_count, system_real_amount, system_vailable_amount) {
+    return {
+      type: AdminContants.ALL_USERS_INFO_SUCCESS,
+      user_count,
+      system_real_amount,
+      system_vailable_amount
     }
   }
 
