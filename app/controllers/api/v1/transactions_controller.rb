62dc84241e9adc2365db94648fc412b4476f84d7
1412350 <ngoc120742@gmail.com>
2018-01-07 15:57:19 +0700

    add api get system confirm transaction

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -40,6 +40,19 @@ class Api::V1::TransactionsController < ApplicationController
     render json: { data: @transactions, total: @transactions.length }, status: 200
   end
 
+  def system_confirmed_transactions
+    @outputs = Output.all.order(id: :desc)
+    @transactions = @outputs.map do |output|
+      {
+        hash: output.output_ref,
+        sender: output.sender,
+        receiver: output.receiver,
+        value: output.amount
+      }
+    end
+    render json: { data: @transactions, total: @transactions.length }, status: 200
+  end
+
   def show
     transaction = @service.get_transaction(params[:id])
     render json: { data: transaction }, status: 200
