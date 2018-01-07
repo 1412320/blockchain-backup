65f806d7bf61b0ee3df881d640ec352a4af14e8e
1412320 <quocminh97@gmail.com>
2018-01-08 01:23:35 +0700

    Fix transaction detail

--- app/javascript/components/Home/TransactionsTable.tsx
+++ app/javascript/components/Home/TransactionsTable.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import { Row, Col, Button, Card, CardText,
          CardBody, CardTitle, CardSubtitle, Table } from 'reactstrap';
+import { Link } from 'react-router-dom';
 import { TransactionInfo } from '../../containers';
-         
+
 export interface TransactionsTableProps {
   transactions: Array<TransactionInfo>,
   page: number
@@ -25,7 +26,7 @@ export class TransactionsTable extends React.Component< TransactionsTableProps ,
         <thead>
           <tr>
             <th className="col-1">#</th>
-            <th className="col-3">Hash</th> 
+            <th className="col-3">Hash</th>
             <th className="col-3">Sender</th>
             <th className="col-3">Reciever</th>
             <th className="col-2">Amount</th>
@@ -36,7 +37,7 @@ export class TransactionsTable extends React.Component< TransactionsTableProps ,
           this.props.transactions.map((e, i) => (
             <tr key={i}>
               <th scope="row" className="col-1">{10*(this.props.page-1) + i + 1}</th>
-              <td className="col-3">{e.hash.slice(0, 20)}...</td> 
+              <td className="col-3"><Link to={"/transactions/" + e.hash}>{e.hash.slice(0, 20)}...</Link></td>
               <td className="col-3">{e.sender.slice(0, 20)}...</td>
               <td className="col-3">{e.receiver.slice(0, 20)}...</td>
               <td className="col-2">{e.value}</td>
