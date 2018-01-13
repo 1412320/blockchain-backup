0b53541e2fd722ace47f866f906f7ecdd9cb4ea5
1412320 <quocminh97@gmail.com>
2018-01-14 00:01:16 +0700

    Apply iterator pattern

--- app/controllers/api/v1/admin_controller.rb
+++ app/controllers/api/v1/admin_controller.rb
@@ -1,30 +1,40 @@
 class Api::V1::AdminController < ApplicationController
   before_action :has_permission_to_do
+  require 'array_iterator'
+
   def all_user_infos
-    @users = User.where("id != ?", current_user.id )
+    @users = User.where("id != ?", current_user.id)
     users_info = Array.new
-    @users.each do |user|
+    
+    i = ArrayIterator.new(@users)
+
+    while i.has_next?
       users_info.push({
-        email: user.email,
-        address: user.wallet.address,
-        real_amount: user.wallet.real_amount,
-        available_amount: user.wallet.available_amount,
+        email: i.item.email,
+        address: i.item.wallet.address,
+        real_amount: i.item.wallet.real_amount,
+        available_amount: i.item.wallet.available_amount,
       })
+      i.next_item
     end
+
     page = params[:page_number].to_i
-    p page
     render json: users_info[10*(page-1),10]
   end
+
   def system_info
     user_count = User.where("id != ?", current_user.id).count
     system_real_amount = Output.real_amount.sum(:amount)
     system_available_amount = Output.available_amount.sum(:amount)
     render json: {user_count: user_count, system_real_amount: system_real_amount, system_available_amount: system_available_amount}
   end
+
   private
+
   def has_permission_to_do
     if (current_user.role != 0)
       render json: {errors: "You don't have permission to do this action!"}, status: 401
     end
   end
+
 end
--- app/services/kcoin_service.rb
+++ app/services/kcoin_service.rb
@@ -1,5 +1,7 @@
 require ('crawlers/kcoin_crawler')
 require ('kcoin_transaction')
+require 'array_iterator'
+
 class KcoinService
   def initialize(user)
     @user = user
@@ -42,11 +44,15 @@ class KcoinService
     @transactions = []
     data = KcoinCrawler.get_data("/blocks", order: -1, limit: @per_page)
     blocks = KcoinCrawler.parse_json(data)
-    blocks.each do |block|
-      block['transactions'].each do |transaction|
-        process_transaction(transaction)
+    i1 = ArrayIterator.new(blocks)
+    while i1.has_next?
+      i2 = ArrayIterator.new(i1.item['transactions'])
+      while i2.has_next?
+        process_transaction(i2.item)
         return @transactions if @transactions.length >= @per_page
+        i2.next_item
       end
+      i1.next_item
     end
     @transactions
   end
@@ -54,17 +60,21 @@ class KcoinService
   private 
   def process_transaction(transaction)
     sender = KcoinTransaction.find_sender(transaction['inputs'][0])
-    transaction['outputs'].each do |output|
-      receiver = output['lockScript'].split(' ')[1]
+
+    i = ArrayIterator.new(transaction['outputs'])
+    while i.has_next?
+      receiver = i.item['lockScript'].split(' ')[1]
       trans = {
         hash: transaction['hash'],
         sender: sender,
         receiver: receiver,
-        value: output['value']
+        value: i.item['value']
       }
       @transactions << trans
       break if @transactions.length >= @per_page
+      i.next_item
     end
+    
   end
 
   def init_transaction
